85faf84bd0b4bed3b524ccb81fbeda65567e1857
Ankit Raj <48787278+ankiiitraj@users.noreply.github.com>
2019-08-05 16:17:58 +0530

    added stylesheet and Javascript in their files rather than in Index.html

--- js/particles.js
+++ js/particles.js
@@ -1,7 +1,24 @@
+/*Smooth Scroll on clicking Nav Links*/
+window.onscroll = function(){"use strict"; if(document.body.scrollTop >= 40 || document.documentElement.scrollTop >= 40 || e.classList.contains("open")){document.getElementById('nav').style['background-color'] = '#000000';}else{document.getElementById('nav').style.backgroundColor = '';} if(document.body.scrollTop >= 3*window.innerHeight || document.documentElement.scrollTop >= 3*window.innerHeight){let item = document.querySelectorAll('.active'); item[0].className = 'single';var items = document.querySelectorAll('.single'); items[3].className = 'active';}else if(document.body.scrollTop >= 2*window.innerHeight || document.documentElement.scrollTop >= 2*window.innerHeight){var item = document.querySelectorAll('.active'); item[0].className = 'single';var items = document.querySelectorAll('.single'); items[2].className = 'active';}else if(document.body.scrollTop >= 1*window.innerHeight || document.documentElement.scrollTop >= 1*window.innerHeight){var item = document.querySelectorAll('.active'); item[0].className = 'single';var items = document.querySelectorAll('.single'); items[1].className = 'active';}else{var item = document.querySelectorAll('.active'); item[0].className = 'single';var items = document.querySelectorAll('.single'); items[0].className = 'active';}}
+
+/*Change Color of active links on Nav Bar*/
+let tar = document.getElementById("target");let e = document.getElementById("toggle"); let nelem = document.getElementById("nav");
+function active(a){let item = document.querySelectorAll('.active'); item[0].className = 'single'; a.className = 'active';
+document.querySelector("#contact").scrollIntoView({behavior: "smooth"}); e.classList.remove("open"); tar.classList.add("nav-items"); tar.classList.remove("toggeled"); /*nelem.style.backgroundColor='';*/}
+
+/*Changing Institune name*/
+let text = ["IIIT Ranchi", "Indian Institute of Informaiton Technology Ranchi", "भारतीय सूचना प्रौद्योगिकी संस्थान रााँची"];
+var counter = 1;var elem = document.getElementById("changeText");setInterval(change, 2500);function change(){elem.classList.add('hide');setTimeout(function () {elem.innerHTML = text[counter]; elem.classList.remove('hide'); counter++;if (counter >= text.length) {counter = 0;}}, 500);}
+
+/*part of Changing color of NavBar from transparent to Black on scrool*/
+function tog(e){if(!(e.classList.contains("open"))){e.classList.add("open"); tar.classList.remove("nav-items"); tar.classList.add("toggeled"); nelem.style['background-color'] = '#000000';} else{e.classList.remove("open"); tar.classList.add("nav-items"); tar.classList.remove("toggeled"); if(document.body.scrollTop < (window.innerHeight -500) && document.documentElement.scrollTop < (window.innerHeight -500)){ nelem.style.backgroundColor='';}}}
+
+/*Particle Interaction Animation*/
 var canvas = document.getElementById("canvas");
 var pen = canvas.getContext("2d");
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
+console.log(canvas.width, canvas.height);
 var dots = [];
 
 class dot{
@@ -27,8 +44,9 @@ class dot{
 	{
 		this.x += this.xSpeed;
 		this.y += this.ySpeed;
-		if(this.x >= canvas.width/2)
+		if(this.x >= canvas.width/2){
 			this.xSpeed -= 0.3;
+		}
 		else
 			this.xSpeed += 0.3;
 		this.colorR = Math.random()*225;
@@ -74,6 +92,7 @@ function interact()
 
 function setup()
 {
+	console.log(1);
 	var numberOfParticles = Math.floor(mapThese(window.innerWidth, 350, 1400, 30, 100));
 	for(var i = 0; i < numberOfParticles; ++i)
 		dots.push(new dot());
@@ -88,6 +107,7 @@ function draw()
 		if(dots[i].x < 0 || dots[i].x > canvas.width || dots[i].y < 0 || dots[i].y > canvas.height)
 			count++;
 	}
+	console.log(count);
 	pen.clearRect(0, 0, canvas.width, canvas.height);
 	for(var i = 0; i < dots.length; ++i)
 	{
---
+++ js/script.js
@@ -0,0 +1,110 @@
+
+let dots = [];
+var canvasDiv = document.getElementById('home');
+var w = canvasDiv.clientWidth - 10;
+var h = canvasDiv.clientHeight -10;
+
+function setup() {
+  var canvas = createCanvas(w, h);
+  canvas.parent('home');
+  var no_of_particles = map(windowWidth, 320, 1500, 20, 100)
+  console.log(no_of_particles);
+  for (let i = 0; i < no_of_particles; i++) {
+    dots.push(new dot());
+  }
+}
+
+function draw() {
+    clear();
+    for (let i = 0; i < dots.length; i++) {
+    dots[i].display();
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].check();
+  }
+  lined();
+  
+}
+
+function lined()
+{
+//    let dict = [];
+//    for(var i = 0; i < dots.length; ++i){
+//    	var foo = {};
+//		foo = {'x': dots[i].x, 'y' : dots[i].y};
+//		dict.push(foo);
+//	}
+//	dots.sort((a, b) => a.x - b.x);
+//	for(var i = 0; i < dots.lenght; ++i){
+//		//var xsq = dots[i].x * dots[i].x;
+//		//var ysq = dots[i].y * dots[i].y;
+//        for(var j = i +1; j < dots.length; ++j){
+//			console.log("dots");
+//			var distance = dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+//			let wt = map(distance, 0, 150, 1, 0);
+//			strokeWeight(wt);
+//			stroke(225);
+//			line(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+//		}
+//    }
+	var count = 0;
+	dots.sort((a, b) => a.x - b.x)
+	for(var i = 0; i < dots.length; ++i){
+        for(var j = i + 1;(j < dots.length && dots[j].x <= (dots[i].x +150)); ++j){
+            var distance = dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+			count++;
+            if(distance <= 150){
+                let wt = map(distance, 0, 150, 1, 0);
+                strokeWeight(wt);
+                stroke(225);
+                line(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+            }
+        }
+    }
+//	var count1 = 0;
+//    for(var i = 0; i < dots.length; ++i){
+//        for(var j = i + 1; j < dots.length; ++j){
+//            var distance = dist(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+//			count1++;
+//            if(distance <= 150){
+//                let wt = map(distance, 0, 150, 1, 0);
+//                strokeWeight(wt);
+//                stroke(225);
+//                line(dots[i].x, dots[i].y, dots[j].x, dots[j].y);
+//            }
+//        }
+//    }
+//	console.log(count1);
+}
+
+
+class dot {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.xSpeed = random(-1, 1);
+    this.ySpeed = random(0, 4);
+    this.colourR = random(0, 225);
+    this.colourB = random(0, 225);
+    this.colourG = random(0, 225);
+  }
+
+  display() {
+    fill(this.colourR, this.colourB, this.colourG);
+    ellipse(this.x, this.y, 3, 3);
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }
+  check()
+  {
+    if(this.x > width || this.x < 0){
+      this.x = random(0, width);
+      this.y = random(-30, 0);
+    }
+    else if(this.y > height)
+    {
+      this.x = random(0, width);
+      this.y = random(-30, 0);
+    }
+  }
+}
